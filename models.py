33785786929c8b467fbebbbfdaca9cc8db5a058e
Grace-Waceke <gracewacekemwangi@gmail.com>
2025-09-22 22:54:42 +0300

    Fix linting issues

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String,  DateTime, func, Numeric, CheckConstraint, Index
+from sqlalchemy import Column, Integer, String, DateTime, func, Numeric, CheckConstraint, Index
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
